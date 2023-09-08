a08f6559f232b2ea5315847ed9fd765734b0e888
koserumeysa <rumeysakose1010@gmail.com>
2023-09-08 11:31:14 +0300

    Add POST mmethod.

--- news/api/views.py
+++ news/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view #decorator
 from news.models import Essay
 from news.api.serializers import EssaySerializer
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def essay_list_create_api_view(request):
     if request.method == 'GET':
         essays = Essay.objects.filter(isActive=True)
@@ -13,5 +13,17 @@ def essay_list_create_api_view(request):
         serializer = EssaySerializer(essays, many=True)
         #many=True is for serializing multiple objects.
         return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = EssaySerializer(data=request.data)
+        #request.data is for getting the data from the request.
+        serializer.is_valid()
+        #This is for checking whether the data is valid or not.
+        if serializer.is_valid():
+            serializer.save()
+            #This is for saving the data.
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            #HTTP_201_CREATED is for creating the data.
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
