4abcf92be4dba4b67c43a5f1d1c9fe558a3c12d8
koserumeysa <rumeysakose1010@gmail.com>
2023-09-08 11:46:41 +0300

    Put method was added.

--- news/api/urls.py
+++ news/api/urls.py
@@ -3,4 +3,5 @@ from news.api import views as api_views
 
 urlpatterns = [
     path('news/',api_views.essay_list_create_api_view, name='essay-list'),
+    path('news/<int:pk>/',api_views.essay_detail_api_view, name='essay-detail')
 ]
--- news/api/views.py
+++ news/api/views.py
@@ -26,4 +26,35 @@ def essay_list_create_api_view(request):
             #HTTP_201_CREATED is for creating the data.
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET', 'PUT', 'DELETE'])
+def essay_detail_api_view(request, pk):
+    essay_instance = Essay.objects.get(pk=pk)
+    #This is for getting the object with the primary key.
+    try:
+        essay_instance = Essay.objects.get(pk=pk)
+    except Essay.DoesNotExist:
+        return Response(
+            {'error':{
+            'code':404,
+            'message': f'Essay which has {{pk}} id was not found!'
+        }}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = EssaySerializer(essay_instance)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = EssaySerializer(essay_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        essay_instance.delete()
+        return Response(
+            {'error':{
+            'code':204,
+            'message': f'Essay which has {{pk}} id was deleted!'
+        }},status=status.HTTP_204_NO_CONTENT)
+    
